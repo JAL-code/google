88fe249ac6357f2fe8e6d3bfe5e82131e60c0289
Joseph <70151426+JAL-code@users.noreply.github.com>
2021-05-05 17:26:24 -0400

    Fix errors with finding all bracketed text.
    Revised code to reduce error.
    1.  main_loop() was not finding all instances of bracketed text.
    2. Without a closing header, the final stage will generate an index error when looking up the found_highlights list.
    3. Not all of the found_highlights were being added to the list.

--- study_tools/class_highlights_list_lines.py
+++ study_tools/class_highlights_list_lines.py
@@ -2,7 +2,7 @@
 
 from access import get_location_for_code
 import re
-import os
+# import os
 
 # this line is only used to pass text to checks
 STAGE_ONE_TEST = False      # Test finding closed brackets on one line
@@ -10,12 +10,13 @@ STAGE_TWO_TEST = False      # Test finding closed brackets on multilines
 STAGE_THREE_TEST = False    # View found highlights and quantify
 STAGE_FOUR_TEST = False     # View the headers found
 STAGE_FIVE_TEST = False     # Check range of highlights verses orginal file
-STAGE_SIX_TEST = True       # Toggle the printing of files to directory.
+STAGE_SIX_TEST = False       # Toggle the printing of files to directory.
 
 def main_loop(line):
 
     """ Stage ONE: Only check_closed can save to found_highlights. """
-    pattern = r"\[[\w\.\*\"\: \?\'\,]*\]"
+    pattern = r"\[[\w.\*\"\: ?\'\,\-\n]*\]"
+    # old r"\[[\w\.\*\"\: \?\'\,]*\]"
     found_closed = re.search(pattern, line)
 
     if STAGE_ONE_TEST:
@@ -26,8 +27,10 @@ def main_loop(line):
         # print(found_closed)
         return False, found_closed[0]
 
-    """ Stage TWO: Only check_open_right_bracket can save temp_line for append to next line. """
-    pattern = r"\[[\w.\*\"\: ?\'\,]*"
+    """ Stage TWO: Only check_open_right_bracket can save temp_line 
+    for append to next line. """
+    pattern = r"\[[\w.\*\"\: ?\'\,\-\n]*"
+    # old r"\[[\w.\*\"\: ?\'\,]*"
     open_right = re.search(pattern, line)
     if open_right:
         if STAGE_TWO_TEST:
@@ -39,33 +42,57 @@ def main_loop(line):
 
 def main():
     
-    file_path = get_location_for_code()    
-    file_name = 'W1-Q2-NO.txt'
+    # User needs to provide root location for the github folder.
+    # For this file, another module was added to provide the string.
+    # See next line VVVVVVVVVV
+    file_path = get_location_for_code()
+    # ^^^^^^^^^^^^^^^^^^^^^^^^
+    git_folder = 'google/Google It-Cert-Automation/'
+    folder_name = 'e-Configuration-Management-and-the-Cloud/W1/'
+    file_name = 'W1-Q1-NO.txt'
     file_group = []
 
     # Save the highlights
     found_highlights = []
-
-    with open(f"{file_path}{file_name}") as file_object:
+    primary_folder = f"{file_path}{git_folder}{folder_name}"
+    load_name = f"{primary_folder}{file_name}"
+    with open(load_name) as file_object:
         add_next_line = False
         # this line is only used to save text to search with next line
         search_with_previous = ""
-        
+
         note_row = 0
         # load the file into a list
         lines = file_object.readlines()
         for line in lines:
             """ Find the next section. """
-            file_header = r"^\[\*Caption:\"(?P<title>[\w\' ]+)\"\:(?P<unit>[\w]+)\*\]"
-            found_header = re.search(file_header, line)
+            file_header = r"""^\[\*Caption:
+                                 \"(?P<title>[\w\'\?\- ]+)\"
+                                 \:(?P<unit>[\w]+)\*\]"""
+            found_header = re.search(file_header, line, re.X)
             if found_header:
+                print(f"Found header: {line}")
                 remove_punc = re.sub(r"[']", '', found_header.group('title'))
-                remove_space = re.sub(r"[ ]", '_', remove_punc)
+                remove_question = re.sub(r"[?]", '_', remove_punc)
+                remove_space = re.sub(r"[ ]", '_', remove_question)
+                
                 # Create a file name
-                file_dict = {'name': f"{file_name[:6]}{found_header.group('unit')}-{remove_space}.txt", 'start': len(found_highlights) + 1, 'end': 0}
+                name_seq = f"{file_name[:6]}{found_header.group('unit')}"
+                file_out = f"{name_seq}-{remove_space}.txt"
+                file_dict = {'name': file_out, 
+                             'start': len(found_highlights) + 1, 'end': 0}
                 if STAGE_FOUR_TEST:
                     print(f"Current saved line: {len(found_highlights)}")
                 file_group.append(file_dict)
+                
+            if len(file_group) > 1:
+                file_group[len(file_group)-2]['end'] = file_group[len(file_group)-1]['start'] - 2
+                print(f"Current lenght: {len(file_group)}")
+                print(file_group[len(file_group)-2]['start'])
+                print(file_group[len(file_group)-2]['end'])
+                print(file_group[len(file_group)-1]['start'])
+                print(file_group[len(file_group)-1]['end'])
+                print(f"End test: {file_group[len(file_group)-1]['end']}")
             if STAGE_TWO_TEST:
                 print(f"Add: {add_next_line}")
             sline = re.sub(r"[\n]", " ", line)
@@ -85,7 +112,7 @@ def main():
                     add_next_line = False
                     search_with_previous = ""
                     found_highlights.append(action_text)
-                    
+
                 # Save line to append to next line
                 if action:
                     if STAGE_TWO_TEST:
@@ -93,7 +120,7 @@ def main():
                     add_next_line = True
                     search_with_previous = action_text
                 # Do nothing
-                
+
             action_text = ""
             note_row += 1
 
@@ -106,23 +133,28 @@ def main():
     fh_count = 0
     for file_header in file_group:
         if fh_count == len(file_group)-1:
-            file_header['end'] = len(found_highlights)-1
+            # file_header['end'] = len(found_highlights)-1
             print(file_header['end'])
         else:
-            file_header['end'] = file_group[fh_count+1]['start']-2
+            # file_header['end'] = file_group[fh_count+1]['start']-2
             print(file_header['end'])
+        s_loc = file_header['start']
+        e_loc = file_header['end']
         if STAGE_FIVE_TEST:
-            print(f"File header: {file_header['name']}")
-            print(f"First: {file_header['start']} - {found_highlights[file_header['start']]}")
-            print(f"End: {file_header['end']} - {found_highlights[file_header['end']]}")
+            if not e_loc == 0:
+                print(f"File header: {file_header['name']}")
+                minus_start = f"{found_highlights[file_header['start']]}"
+                print(f"First: {file_header['start']} - {minus_start}")
+                minus_end = {found_highlights[file_header['end']]}
+                print(f"End: {file_header['end']} - {minus_end}")
         fh_count += 1
         if STAGE_SIX_TEST:
-            with open(f"{file_path}{file_header['name']}", 'w') as sub_file_object:
-                for highlight in found_highlights[file_header['start']:file_header['end']]:
-                    
-                    sub_file_object.write(f"{highlight}\n")
-                sub_file_object.close()
-        
+            # sub video file = pf_name
+            pf_name = f"{primary_folder}{file_header['name']}"
+            if not e_loc == 0:
+                with open(pf_name, 'w') as sub_file_object:
+                    for highlight in found_highlights[s_loc:e_loc]:
+                        sub_file_object.write(f"{highlight}\n")
+                    sub_file_object.close()
 
-            
 main()
