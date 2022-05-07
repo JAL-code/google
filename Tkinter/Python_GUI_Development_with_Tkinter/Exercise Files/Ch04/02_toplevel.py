1f438af1f32524a11fe816519909ae13140f5a39
JAL-code <70151426+JAL-code@users.noreply.github.com>
2022-05-07 05:28:18 -0400

    Update 02_toplevel.py
    Included commits explaining how to work with windows.

--- Tkinter/Python_GUI_Development_with_Tkinter/Exercise Files/Ch04/02_toplevel.py
+++ Tkinter/Python_GUI_Development_with_Tkinter/Exercise Files/Ch04/02_toplevel.py
@@ -7,24 +7,24 @@ from tkinter import *
 root = Tk()
 
 window = Toplevel(root)
-window.title('New Window')
+window.title('New Window')  # Default size is x = 200, y = 200
 
-window.lower()
-window.lift(root)
+window.lower() # move the window down one position
+window.lift(root) # move the window up one position
 
-window.state('zoomed')
-window.state('withdrawn')
-window.state('iconic')
-window.state('normal')
-print(window.state())
-window.state('normal')
+window.state('zoomed')  # Maximize the window to largest allowable size
+window.state('withdrawn') # Hide a window
+window.state('iconic')  # Minimize the window so it is still accessible in the task bar.
+window.state('normal')  # Default state in zoomed state.
+print(window.state()) # Get the current state of a window.  It's still zoomed.
+window.state('normal') # Return the window initalization state.
 
-window.iconify()
-window.deiconify()
+window.iconify()  # Shortcut to put the window in the task bar.
+window.deiconify()  # Shortcut to put the window in normal state.
 
-window.geometry('640x480+50+100')
+window.geometry('640x480+50+100')  # width x height + (50 left + 100 up) origin (x+ ->, y+ V)
 print(window.geometry())
-window.resizable(False, False)
+window.resizable(False, False)  # control resize in x and y directions
 window.maxsize(640, 480)
 window.minsize(200, 200)
 window.resizable(True, True)
