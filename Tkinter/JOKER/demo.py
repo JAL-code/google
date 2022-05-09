0353f89b28c9ea65ebdde6287975fa6a5cc3ad9b
JAL-code <70151426+JAL-code@users.noreply.github.com>
2022-05-09 16:56:10 -0400

    Update demo.py
    Seed file for object based design.

--- Tkinter/JOKER/demo.py
+++ Tkinter/JOKER/demo.py
@@ -1,1 +1,16 @@
 # Demo file for new GUI using Tkinter (Python 3.5+)
+# Updated class based seed file per JAL-code
+
+from tkinter import *
+from tkinter import ttk
+
+class MainGui:
+
+    def __init__(self, root):
+        root.grid()
+        print("Started Container")
+
+if __name__ == "__main__":
+    root = Tk()
+    MainGui(root)
+    root.mainloop()
