9b0c65857e40b99279ab3de82e5e9f2931c25f43
JAL-code <70151426+JAL-code@users.noreply.github.com>
2022-06-06 07:29:38 -0400

    Update 01_callback_live.py
    Updated callback to send value of button with a lambda function.

--- Tkinter/Python_GUI_Development_with_Tkinter/Exercise_Files/Ch07/01_callback_live.py
+++ Tkinter/Python_GUI_Development_with_Tkinter/Exercise_Files/Ch07/01_callback_live.py
@@ -2,14 +2,21 @@
 # template.py by Barron Stone
 # This is an exercise file from Python GUI Development with Tkinter on lynda.com
 
+# Callbacks only available for specific actions.
+# Tkinter can bind to events with specific handlers
+# This exercise focuses on callbacks.
+
 from tkinter import *
 from tkinter import ttk        
     
 root = Tk()
+# Command callbacks functions: Button, Checkbutton, Radiobutton, Spinbox, Scale, Scrollbar
+def callback(number):
+    print(f'In the callback {number}')
 
-def callback():
-    print('In the callback')
-
-ttk.Button(root, text = "Click Me!", command = callback).pack()
+# Lambda keyword creates an anonymous function that contains the callback method.
+ttk.Button(root, text = "Click Me 1", command = lambda: callback(1)).pack()
+ttk.Button(root, text = "Click Me 2", command = lambda: callback(2)).pack()
+ttk.Button(root, text = "Click Me 3", command = lambda: callback(3)).pack()
 
 root.mainloop()
