329d02890ed073c46f87bf7702e1dbb04d7afb50
JAL-code <70151426+JAL-code@users.noreply.github.com>
2022-06-11 02:06:38 -0400

    Create 02_keyboard_live.py
    Basic events types

---
+++ Tkinter/Python_GUI_Development_with_Tkinter/Exercise_Files/Ch07/02_keyboard_live.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# template.py by Barron Stone
+# This is an exercise file from Python GUI Development with Tkinter on lynda.com
+
+# Tk Event Types
+# ButtonPress, ButtonRelease, Enter, Leave, Motion,
+# KeyPress Events
+#     Event Format  : Event Description
+# <Key>, <KeyPress> : User pressed any key.
+# <KeyPress-Delete> : User pressed Delete key.
+# <KeyRelease-Right>: User released the Right Arrow key.
+# a, b, c, 1, 2, 3, etc... and <space>, <less>: User pressed a printable key.
+# <Shift_L>, <Control_R>, <F5>, <Up> : User pressed a "special" key.
+#  Note keysym encased in angle brackets.
+# <Return>: User pressed the enter key.
+# <Control-Alt-Next> :  User pressed Ctrl+Alt+Page Down keys.
+# KeyRelease
+# FocusIn
+# FocusOut
+
+from tkinter import *
+from tkinter import ttk        
+    
+root = Tk()
+
+def key_press(event):
+    print('type:{}'.format(event.type))
+    print('widget: {}'.format(event.widget))
+    print('char: {}'.format(event.char))
+    print('keysym: {}'.format(event.keysym))
+    print('keycode: {}'.format(event.keycode))
+
+root.bind('<KeyPress>', key_press)
+
+root.mainloop()
