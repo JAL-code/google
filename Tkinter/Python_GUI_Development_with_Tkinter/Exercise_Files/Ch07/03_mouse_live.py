c4a086cc508f029aa3fe395e08809cb0ac1cc6bd
JAL-code <70151426+JAL-code@users.noreply.github.com>
2022-06-16 08:10:18 -0400

    Create 03_mouse_live.py
    Added live creation of 03 mouse.

---
+++ Tkinter/Python_GUI_Development_with_Tkinter/Exercise_Files/Ch07/03_mouse_live.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# template.py by Barron Stone
+# This is an exercise file from Python GUI Development with Tkinter on lynda.com
+
+from tkinter import *
+from tkinter import ttk        
+
+# KeyPress Events
+#     Event Format  : Event Description
+# <Button>, <ButtonPress> : Any button was pressed.
+# <Button-1>, <ButtonPress-1>, <1> : Button 1 was pressed.
+# <ButtonRelease-1> : Button 1 was released.
+# <Double-Button-1> or <Triple-Button-1> : Button 1 was double- or triple-clicked.
+# Movement-Related Mouse Events
+# <Enter> : Mouse entered widget area.
+# <Leave> : Mouse left widget area.
+# <Motion> : Mouse was moved.
+# <B1-Motion> : Mouse was moved with Button 1 held down.
+
+def mouse_press(event):
+    global prev # save the event so accessible next mouse event.
+    prev = event
+    print('type:{}'.format(event.type))
+    print('widget: {}'.format(event.widget))
+    print('num: {}'.format(event.num))
+    print('x: {}'.format(event.x))
+    print('y: {}'.format(event.y))
+    print('x_root: {}'.format(event.x_root))
+    print('y_root: {}'.format(event.y_root))
+
+root = Tk()
+
+
+canvas = Canvas(root, width = 640, height = 480, background = 'white')
+canvas.grid()
+
+def draw(event):
+    global prev
+    canvas.create_line(prev.x, prev.y, event.x, event.y, width=5)
+
+canvas.bind('<ButtonPress>', mouse_press)
+canvas.bind('<B1-Motion>', draw)
+
+root.mainloop()
